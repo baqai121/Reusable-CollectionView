29e70dd85389c7c37e48f8a1cd434a78ed7aa5ab
Daniyal Yousuf <daniyal.yousuf@koderlabs.com>
2019-01-31 18:11:24 +0500

    Initial Commit

---
+++ CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by Daniyal Yousuf on 1/31/19.
+//  Copyright © 2019 Daniyal Yousuf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionView
+//
+//  Created by Daniyal Yousuf on 1/31/19.
+//  Copyright © 2019 Daniyal Yousuf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
